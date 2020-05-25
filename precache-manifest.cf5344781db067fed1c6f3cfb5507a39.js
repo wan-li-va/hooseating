214d69c563c9b143a89a172607ee9975998adb02
Wan Li <wl9wgc@virginia.edu>
2020-05-25 14:27:53 -0400

    Updates

---
+++ precache-manifest.cf5344781db067fed1c6f3cfb5507a39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5775e1d87fe263671ffddf0cee1b8fbc",
+    "url": "/hooseating/index.html"
+  },
+  {
+    "revision": "30504026ecf51f19b812",
+    "url": "/hooseating/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2e91d07da6ae9171938",
+    "url": "/hooseating/static/js/2.2807bf03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hooseating/static/js/2.2807bf03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30504026ecf51f19b812",
+    "url": "/hooseating/static/js/main.8cd2c938.chunk.js"
+  },
+  {
+    "revision": "f3cbdcb6e4ae7e989030",
+    "url": "/hooseating/static/js/runtime-main.2010144b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hooseating/static/media/logo.5d5d9eef.svg"
+  }
+]);
