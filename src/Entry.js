4b7fce9caaa052b2a4b1ae5a5192b06457db6e16
Wan Li <wl9wgc@virginia.edu>
2020-05-27 13:47:37 -0400

    cards working now

--- src/Entry.js
+++ src/Entry.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import Card from "react-bootstrap/Card";
+import Card from "react-bootstrap/Card";
 
 export default class Entry extends Component {
   constructor(props) {
@@ -13,7 +13,7 @@ export default class Entry extends Component {
     return (
       <div>
         <div className="Entry">
-          <p>Name: {this.props.restaurant.name} </p>
+          {/* <p>Name: {this.props.restaurant.name} </p>
           <p>Rating: {this.props.restaurant.rating}</p>
           {(this.props.restaurant.price_level === 1) ?
             <p>Price: $</p>
@@ -23,25 +23,25 @@ export default class Entry extends Component {
               :
               <p>Price: $$$</p>
             )
-          }
+          } */}
 
-          {/* <Card>
+          <Card>
             <Card.Body>
               <Card.Text>
-                <p>Name: {this.props.restaurant.name} </p>
-                <p>Rating: {this.props.restaurant.rating}</p>
+                <em>Name: {this.props.restaurant.name} </em>
+                <em>Rating: {this.props.restaurant.rating}</em>
                 {(this.props.restaurant.price_level === 1) ?
-                  <p>Price: $</p>
+                  <em>Price: $</em>
                   :
                   ((this.props.restaurant.price_level === 2) ?
-                    <p>Price: $$</p>
+                    <em>Price: $$</em>
                     :
-                    <p>Price: $$$</p>
+                    <em>Price: $$$</em>
                   )
                 }
               </Card.Text>
             </Card.Body>
-          </Card> */}
+          </Card>
         </div>
       </div>
     );
