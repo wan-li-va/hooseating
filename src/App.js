048d5efc03011244fe999c2795686d13d807d03c
Wan Li <wl9wgc@virginia.edu>
2020-05-27 09:27:38 -0400

    restaurant search works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import Restaurants from "./Restaurants.js";
 import Input from "./Input.js";
 import Map from "./Map.js";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Places from "./Places.json";
+import axios from 'axios';
 
 export default class App extends Component {
   constructor(props) {
@@ -16,35 +16,15 @@ export default class App extends Component {
   }
 
   componentDidMount = () => {
-    const axios = require("axios").default;
+    // axios = require("axios");
 
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.0293,-78.4767&radius=30&type=restaurant&key=AIzaSyBQRbOl8Z5HnrY12zURP84C6Tdwsoy-HUI"
-      )
-      .then(function (response) {
-        console.log(response);
-      });
-
-    // var latitude = 38.0293;
-    // var longitude = -78.4767;
-
-    // const url =
-    //   "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
-    //   "location=" +
-    //   latitude +
-    //   "," +
-    //   longitude +
-    //   "&radius=30&type=restaurant&key=AIzaSyBQRbOl8Z5HnrY12zURP84C6Tdwsoy-HUI";
-
-    // fetch(url)
-    //   .then((response) => response.json())
-    //   .then((response) =>
-    //     this.setState({
-    //       restaurants: response.results,
-    //     })
-    //   );
-  };
+    axios.get(
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.0293,-78.4767&radius=30&type=restaurant&key=AIzaSyBQRbOl8Z5HnrY12zURP84C6Tdwsoy-HUI")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ restaurants: response.data.results })
+      })
+  }
 
   render() {
     return (
