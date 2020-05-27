8025f60b766616c4b5d0f307d313b81572f3b4ea
Wan Li <wl9wgc@virginia.edu>
2020-05-27 13:37:21 -0400

    add card implementation in comments

--- src/Entry.js
+++ src/Entry.js
@@ -24,6 +24,24 @@ export default class Entry extends Component {
               <p>Price: $$$</p>
             )
           }
+
+          {/* <Card>
+            <Card.Body>
+              <Card.Text>
+                <p>Name: {this.props.restaurant.name} </p>
+                <p>Rating: {this.props.restaurant.rating}</p>
+                {(this.props.restaurant.price_level === 1) ?
+                  <p>Price: $</p>
+                  :
+                  ((this.props.restaurant.price_level === 2) ?
+                    <p>Price: $$</p>
+                    :
+                    <p>Price: $$$</p>
+                  )
+                }
+              </Card.Text>
+            </Card.Body>
+          </Card> */}
         </div>
       </div>
     );
