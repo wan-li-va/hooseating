231836057f785542d65a8636d75015893de94bf9
Alex Kyereboah <atk4kcw@virginia.edu>
2020-05-28 11:46:56 -0400

    added map functionality and updating based on new addresses

--- src/AppMap.js
+++ src/AppMap.js
@@ -1,31 +1,46 @@
 import React, { Component } from "react";
-import { Map, TileLayer, Popup } from 'react-leaflet'
+import { Map, TileLayer, Popup } from "react-leaflet";
 export default class AppMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: this.props.lat,
-      lng: this.props.lng,
-      zoom: 13,
-      restaurants: this.props.restaurants
-    };
-  }
-
-  render() {
-    return (
-      this.state.restaurants ?
+      restaurants: this.props.restaurants,
+      restaurantMap: (
         <Map
-          center={[this.state.lat, this.state.lng]}
-          zoom={this.state.zoom}
-          style={{ width: '100%', height: '80vh' }}
+          center={[this.props.lat, this.props.lng]}
+          zoom={15}
+          style={{ width: "100%", height: "80vh" }}
         >
           <TileLayer
             attribution='copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </Map>
-        :
-        'Data is loading...'
-    );
+      ),
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.lat !== this.props.lat || prevProps.lng !== this.props.lng) {
+      console.log("Component did update");
+      console.log(this.props.lat, this.props);
+      this.setState({
+        restaurantMap: (
+          <Map
+            center={[this.props.lat, this.props.lng]}
+            zoom={15}
+            style={{ width: "100%", height: "80vh" }}
+          >
+            <TileLayer
+              attribution='copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </Map>
+        ),
+      });
+    }
+  }
+  render() {
+    return this.state.restaurantMap;
   }
 }
